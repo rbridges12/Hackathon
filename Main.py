b49181c3fb6c41a0f6ddbb99454a815dcb38c6de
Riley Bridges <rlybrdgs@gmail.com>
2019-03-02 11:39:47 -0600

    Started main

--- Main.py
+++ Main.py
@@ -1,2 +1,5 @@
+import Monopoly.py
+import Player.py
 
-
+player1 = Player("person1", 1000, 0, [])
+player2 = Player("person2", 1000, 0,)
--- Player.py
+++ Player.py
@@ -1,18 +1,23 @@
 class Player:
 
+    def __init__(self, name):
+        self.name = name
+        self.money = 1000
+        self.position = 0
+        self.properties = []
 
-    def __init__(self, money, position, properties):
-        self.money = money
-        self.position = position
-        self.properties = properties
     def move_forward(self, amount):
         self.position += amount
+
     def move_backward(self, amount):
         self.position -= amount
+
     def addMoney(self,amount):
         self.money += amount
+
     def subtractMoney(self,amount):
         self.money -= amount
+
     def getProperties(self, number):
         self.properties = number
         return self.properties
