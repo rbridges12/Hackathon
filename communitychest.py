a6501235d0bac0119d51472f5302e746638beeaa
Rayan20 <raymondyang76@gmail.com>
2019-03-02 10:41:18 -0600

    Edited communitychest and main classes

--- communitychest.py
+++ communitychest.py
@@ -1,8 +1,8 @@
 class CommunityChest:
-    def_init_(self,myspaces,myprice,myname)
-    self.spaces = myspaces
-    self.price = myprice
-    self.name = myname
-    def
+    def __init__(self,myspaces,myprice,myname):
+        self.spaces = myspaces
+        self.price = myprice
+        self.name = myname
+
 
 
