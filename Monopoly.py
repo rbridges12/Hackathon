56c9f62cdcf918c1d8f79397aa94c1939dee7931
Riley Bridges <rlybrdgs@gmail.com>
2019-03-02 10:54:06 -0600

    added 1 property

--- Monopoly.py
+++ Monopoly.py
@@ -1,5 +1,10 @@
+import Property.py
+
+
 class Monopoly:
     def __init__(self, players):
         self.players = players
 
     def __create_properties(self):
+        self.properties = []
+        self.properties.append(Property.Property("Test Property", 500, "blue", 15, 0, 0))
--- Property.py
+++ Property.py
@@ -1,14 +1,16 @@
 class Property:
-    def __init__(self, myname, myprice, mycolor, myrent, myhouses, myhotel, myowner):
+    def __init__(self, myname, myprice, mycolor, myrent, myhouses, myhotel):
         self.name = myname
         self.price = myprice
         self.color = mycolor
         self.rent = myrent
         self.houses = myhouses
         self.hotel = myhotel
-        self.owner = myowner
+        self.owner
+
     def bought(self, player):
         self.owner = player
+
     def buy_house(self, numhouse):
         if (self.houses + numhouse) < 5:
             self.houses = self.houses + numhouse
